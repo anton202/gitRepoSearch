1cba14ab9b0abb31aa033c5413416639909f3bde
anton202 <a.kluge202@gmail.com>
2019-05-01 16:44:36 +0300

    fixed loadin spinner issue

--- client/gitRepoSearch/src/app/search/search.service.ts
+++ client/gitRepoSearch/src/app/search/search.service.ts
@@ -7,14 +7,19 @@ import { Subject } from 'rxjs';
     providedIn: 'root'
 })
 export class SearchService {
+    loading = false;
     gitRepos = new Subject();
     gitApiUrl = 'https://api.github.com/search/repositories?q=';
 
     constructor(private http: HttpClient) { }
 
     getRepos(repoName) {
+        this.loading = true;
         this.http.get(this.gitApiUrl + repoName)
-            .subscribe(repos => {this.gitRepos.next(repos) ; console.log(repos)});
+            .subscribe(repos => {
+                this.loading = false
+                this.gitRepos.next(repos)
+            });
     }
 
 }
