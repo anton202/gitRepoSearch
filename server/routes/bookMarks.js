ec6b396140fcf89ac1fd9605f4bca5ccf8177cef
anton202 <a.kluge202@gmail.com>
2019-05-04 13:35:33 +0300

    beutify

--- server/routes/bookMarks.js
+++ server/routes/bookMarks.js
@@ -10,14 +10,12 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/save',(req,res)=>{
-    
     if(!req.session.bookMarks){
         req.session.bookMarks = [];
         req.session.bookMarks.push(req.body);
     }else{
         req.session.bookMarks.push(req.body)
     }
-    console.log(req.session.bookMarks)
     res.end()
 })
 
@@ -25,14 +23,13 @@ app.delete('/delete/:repoName/:avatar',(req,res)=>{
     const repoName = req.params.repoName;
     const avatar = decodeURIComponent(req.params.avatar);
     const bookMarks = req.session.bookMarks;
-    console.log('gets here')
+   
     bookMarks.forEach((bookMark, idx) => {
         if(bookMark.repoName === repoName && bookMark.avatar === avatar){
             bookMarks.splice(idx,1)
             console.log('deleted')
         }
     });
-
     res.end()
 })
 
